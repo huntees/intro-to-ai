f84dcfd7e142848e444fcc220439d809bb401683
Alain <alainsiow@gmail.com>
2020-10-26 11:26:19 +0000

    Week 4 Correction

--- Week4/q2.py
+++ Week4/q2.py
@@ -20,52 +20,56 @@ def main():
     plt.close()
 
     #Build randomized PCA with 150 components
-    pca = PCA(n_components=150, svd_solver='randomized')
-    pca.fit(faces.data)
+    pca = PCA(n_components=150, whiten=True, random_state=42, svd_solver='randomized')
+    pca.fit(faces['data'])
 
     ##Also, produce a graph plotting how explained variance changes with the number of components
-    # print(pca.explained_variance_)
-    #
-    # def draw_vector(v0, v1, ax=None):
-    #     ax = ax or plt.gca()
-    #     arrowprops = dict(arrowstyle='->',
-    #                       linewidth=2,
-    #                       shrinkA=0, shrinkB=0, color='k')
-    #     ax.annotate('', v1, v0, arrowprops=arrowprops)
-    #
-    # # plot data
-    # plt.scatter(faces.data[:, 0], faces.data[:, 1], alpha=0.2)
-    # for length, vector in zip(pca.explained_variance_, pca.components_):
-    #     v = vector * 3 * np.sqrt(length)
-    #     draw_vector(pca.mean_, pca.mean_ + v)
-    # plt.axis('equal');
+    plt.figure
+    plt.plot(np.cumsum(pca.explained_variance_ratio_))
+    plt.xlabel('number of components')
+    plt.ylabel('cumulative explained variance');
+    plt.show()
+    plt.close()
 
 
     #Then split your data into training and testing.
-    X_train, X_test= train_test_split(faces.data, test_size=0.25, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(faces['data'], faces['target'], random_state=42)
 
     #Transform your training and testing data using your PCA
-    X_train_pro = pca.fit_transform(X_train)
-    X_test_pro = pca.fit_transform(X_test)
+    projection = pca.fit_transform(X_train)
+    projectionTest = pca.fit_transform(X_test)
 
     # print("original shape:   ", X_train.shape)
-    # print("transformed shape:", X_train_pro.shape)
-    # print("original shape:   ", X_test.shape)
-    # print("transformed shape:", X_test_pro.shape)
+    # print("transformed shape:", projection.shape)
 
     # Set up an SVM (it is suggest you use the 'rbf' kernal, gamma=0.001, C=5. Again, investigate changes to these.
-    svm_model = SVC(kernel='rbf', gamma='0.001', C=5)
+    svm_model = SVC(kernel='rbf', class_weight='balanced', gamma='0.001', C=5)
 
     # Fit the model to the transformed data
-    svm_model.fit(X_train_pro)
+    svm_model.fit(projection, y_train)
 
     # Predict the values of the training set
-    X_pred = svm_model.predict(X_train_pro)
-    print(X_test_pro)
-    print(X_pred)
+    y_pred = svm_model.predict(projectionTest)
+    print(y_test)
+    print(y_pred)
 
     # Calculate accuracy.
-    print('Accuracy: %.2f' % accuracy_score(X_test_pro, X_pred))
+    print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+
+    # From SOLUTION
+    # use the classification report to get a more details summary of successes
+    from sklearn.metrics import classification_report
+    print(classification_report(y_test, y_pred, target_names=faces.target_names))
+
+    # show the PCA representations of the faces, and colour
+    # code labels for success or failure.
+    fig, ax = plt.subplots(3, 5)
+    for i, axi in enumerate(ax.flat):
+        axi.imshow(pca.components_[i].reshape(62, 47), cmap='bone')
+        axi.set(xticks=[], yticks=[])
+        axi.set_ylabel(faces.target_names[y_pred[i]].split()[-1],
+                       color='black' if y_pred[i] == y_test[i] else 'red')
+    fig.suptitle('Predicted Names; Incorrect Labels in Red', size=14)
 
 
 if __name__ == '__main__':
